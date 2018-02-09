5563c310b8e65e7ddeb2621f03c390c8535afd95
JackMacWindows <jackbruienne@yahoo.com>
2018-02-09 14:59:09 -0500

    Updated syntax

--- example/test3.s
+++ example/test3.s
@@ -1,10 +1,22 @@
-addn 2,4
-ifeq $0,6
-    outp 84 ; Can be indented
-done
-ifne $0,6
-    outp 70
-done
-stop
-You can put comments at the end of the program.
-Assembly ends once it hits the stop instruction.
+The assembler will ignore anything
+that is put before the first section.
+Note the data and bss sections must
+be put before the text section.
+
+section .data     ; named constants
+    true 84       ; replace all instances of true with 84 (T)
+    false 70      ; replace all instances of false with 70 (F)
+section .bss      ; named variables in memory
+    num 1,2       ; replace all instances of num with $1 and set it to 2
+section .text     ; code
+    add num,4     ; add num & 4
+    ieq $0,6      ; if value 0 == 6
+        out true  ; send value of true to the output
+    end           ; end the if
+    ine $0,6      ; if value 0 != 6
+        out false ; send value of false to the output
+    end           ; end the if
+    ret           ; stop the program
+
+The assembler will also ignore
+anything after the ret instruction.
